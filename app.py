a6cb6e4f76fa8de8ce58a962ad6f3ccf15d1d693
Michael Fitzgerald <michaelfitzgerald@Michaels-MacBook-Pro.local>
2024-09-19 04:31:58 -0400

    Fixed app configuration for Heroku

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, redirect, url_for,send_from_directory
+from flask import Flask, redirect, url_for, send_from_directory
 from inventory import inventory_bp
 from production import production_bp
 from database import init_db
+import os  # Add this import for os
 
 app = Flask(__name__)
 
 # Register blueprints
 app.register_blueprint(inventory_bp, url_prefix='/inventory')
 app.register_blueprint(production_bp, url_prefix='/production')
-
 
 # Add a root route that redirects to the production index page or any other route you want
 @app.route('/')
@@ -24,5 +24,4 @@ def uploaded_file(filename):
 if __name__ == '__main__':
     init_db()  # Initialize the database
     port = int(os.environ.get('PORT', 5000))  # Get the port from the environment
-    app.run(host='0.0.0.0', port=port)  # Bind to 0.0.0.0 to allow access from the web
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)  # Only one app.run
