cad420abe00b02fc1fa2cfc85cc60507e0eb05df
Michael Fitzgerald <michaelfitzgerald@Michaels-MacBook-Pro.local>
2024-09-19 04:43:57 -0400

    Fixed app configuration for Heroku

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def uploaded_file(filename):
 if __name__ == '__main__':
     init_db()  # Initialize the database
     port = int(os.environ.get('PORT', 5000))  # Get the port from the environment
-    app.run(host='0.0.0.0', port=port)  # Only one app.run
+    app.run(host='0.0.0.0', port=port)
